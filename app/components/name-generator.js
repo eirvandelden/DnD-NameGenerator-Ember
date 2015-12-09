95c72418c13506a559fcb2fabb602c5ecb72c983
Etienne van Delden <etienne@kabisa.nl>
2015-12-09 13:31:03 +0100

    Create a new name-generator, that generates a random name
    Based on the Table from the D&D DM’s Screen.
    
    Using lodash to generate random INT’s, to generate a new name each time
    the page is viewed

---
+++ app/components/name-generator.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import _number from 'lodash/number';
+
+var beginning = ['A', 'Be', 'De', 'El', 'Fa', 'Jo', 'Ki', 'La', 'Ma', 'Na', 'O', 'Pa', 'Re', 'Si', 'Ta', 'Va'];
+var middle = ['bar', 'ched', 'dell', 'far'];
+var last = ['a','ac', 'ai', 'al', 'am', 'an', 'ar', 'ea', 'el', 'er', 'ess', 'elt', 'ic','id', 'il', 'in', 'is', 'of', 'us'];
+
+export default Ember.Component.extend({
+    name: Ember.computed('generatedName', function () {
+        var rbeginning = _number.random(0, beginning.length - 1),
+            rmiddle = _number.random(0, middle.length - 1),
+            rlast = _number.random(0, last.length - 1);
+
+        return beginning[rbeginning] + middle[rmiddle] + last[rlast];
+    })
+});
