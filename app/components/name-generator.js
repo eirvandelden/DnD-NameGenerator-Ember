2c716e9c85a1be24c2f2737a29258be2b32df05f
Etienne van Delden <etienne@kabisa.nl>
2015-12-09 14:06:16 +0100

    Regenerate name on click

--- app/components/name-generator.js
+++ app/components/name-generator.js
@@ -6,11 +6,18 @@ var middle = ['bar', 'ched', 'dell', 'far'];
 var last = ['a','ac', 'ai', 'al', 'am', 'an', 'ar', 'ea', 'el', 'er', 'ess', 'elt', 'ic','id', 'il', 'in', 'is', 'of', 'us'];
 
 export default Ember.Component.extend({
-    name: Ember.computed('generatedName', function () {
-        var rbeginning = _number.random(0, beginning.length - 1),
-            rmiddle = _number.random(0, middle.length - 1),
-            rlast = _number.random(0, last.length - 1);
+  actions: {
+    generateName() {
+      var rbeginning = _number.random(0, beginning.length - 1),
+          rmiddle = _number.random(0, middle.length - 1),
+          rlast = _number.random(0, last.length - 1);
 
-        return beginning[rbeginning] + middle[rmiddle] + last[rlast];
-    })
+      var name = beginning[rbeginning] + middle[rmiddle] + last[rlast];
+
+      this.setProperties({'name': name});
+    }
+  },
+  name: Ember.computed('bla', function () {
+    this.sendAction('generateName')
+  })
 });
