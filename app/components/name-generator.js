9329c100f17db4bda4d719be2fabfce8f02eb0d9
Etienne van Delden <etienne@kabisa.nl>
2015-12-10 10:38:23 +0100

    Refactor code to use mostly ES6 syntax

--- app/components/name-generator.js
+++ app/components/name-generator.js
@@ -6,19 +6,20 @@ var middle = ['bar', 'ched', 'dell', 'far'];
 var last = ['a','ac', 'ai', 'al', 'am', 'an', 'ar', 'ea', 'el', 'er', 'ess', 'elt', 'ic','id', 'il', 'in', 'is', 'of', 'us'];
 
 export default Ember.Component.extend({
+  setup: Ember.on('init', function () { this.setName(); }),
   actions: {
     newName() {
-        this.setName();
+      this.setName();
     }
   },
-  generateName: function () {
-      var rbeginning = _number.random(0, beginning.length - 1),
-          rmiddle = _number.random(0, middle.length - 1),
-          rlast = _number.random(0, last.length - 1);
+  generateName() {
+    var rbeginning = _number.random(0, beginning.length - 1),
+        rmiddle = _number.random(0, middle.length - 1),
+        rlast = _number.random(0, last.length - 1);
 
-      return beginning[rbeginning] + middle[rmiddle] + last[rlast];
+    return beginning[rbeginning] + middle[rmiddle] + last[rlast];
   },
-   setName: function () {
-       this.set('name', this.generateName())
-   }.on('init')
+  setName() {
+    this.set('name', this.generateName());
+  }
 });
